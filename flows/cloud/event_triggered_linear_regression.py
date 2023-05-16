3ddeb9fb1a9ba6393cd5793c162b87cad9bb55f8
Makarand Tapaswi <makarand@wadhwaniai.org>
2023-05-16 18:06:24 +0000

    project 3

--- flows/cloud/event_triggered_linear_regression.py
+++ flows/cloud/event_triggered_linear_regression.py
@@ -11,26 +11,23 @@ class TaxiFarePrediction(FlowSpec):
     data_url = Parameter("data_url", default=URL)
 
     def transform_features(self, df):
+        # TODODONE: 
+        # Try to complete tasks 2 and 3 with this function doing nothing like it currently is.
+        # Understand what is happening.
+        # Revisit task 1 and think about what might go in this function.
 
-        # TODO: 
-            # Try to complete tasks 2 and 3 with this function doing nothing like it currently is.
-            # Understand what is happening.
-            # Revisit task 1 and think about what might go in this function.
+        obviously_bad_data_filters = {
+            'fare_amount': df.fare_amount > 0,         # fare_amount in US Dollars
+            'trip_distance_max': df.trip_distance <= 100,    # trip_distance in miles
+            'trip_distance_min': df.trip_distance > 0,
+            'tip_amount': df.tip_amount > 0,
+            'total_amount': df.total_amount > 0,   
+        }
 
-        obviously_bad_data_filters = [
-
-            df.fare_amount > 0,         # fare_amount in US Dollars
-            df.trip_distance <= 100,    # trip_distance in miles
-            df.trip_distance > 0
-
-            # TODO: add some logic to filter out what you decide is bad data!
-            # TIP: Don't spend too much time on this step for this project though, it practice it is a never-ending process.
-
-        ]
-
-        for f in obviously_bad_data_filters:
+        for _, f in obviously_bad_data_filters.items():
             df = df[f]
-            
+            # print(f'Removed {key}, size: {len(df)}')
+
         return df
 
     @step
@@ -41,7 +38,7 @@ class TaxiFarePrediction(FlowSpec):
 
         self.df = self.transform_features(pd.read_parquet(self.data_url))
 
-        # NOTE: we are split into training and validation set in the validation step which uses cross_val_score.
+        # NOTEOK: we are split into training and validation set in the validation step which uses cross_val_score.
         # This is a simple/naive way to do this, and is meant to keep this example simple, to focus learning on deploying Metaflow flows.
         # In practice, you want split time series data in more sophisticated ways and run backtests. 
         self.X = self.df["trip_distance"].values.reshape(-1, 1)
@@ -53,9 +50,8 @@ class TaxiFarePrediction(FlowSpec):
         "Fit a single variable, linear model to the data."
         from sklearn.linear_model import LinearRegression
 
-        # TODO: Play around with the model if you are feeling it.
+        # TODODONE: Play around with the model if you are feeling it.
         self.model = LinearRegression()
-
         self.next(self.validate)
 
     def gather_sibling_flow_run_results(self):
